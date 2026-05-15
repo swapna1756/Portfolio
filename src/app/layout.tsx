ef0764191b2ca2218e7c1d8533d76e894cf893a9
swapna1756 <swapna1756@users.noreply.github.com>
2026-05-15 13:06:30 +0530

    Remove browser tab icon by adding blank favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500&family=Poppins:wght@600;700;800&family=Dancing+Script:wght@700&display=swap" rel="stylesheet" />
+        <link rel="icon" href="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1 1'%3E%3C/svg%3E" />
       </head>
       <body className="font-body antialiased selection:bg-primary/20">{children}</body>
     </html>
